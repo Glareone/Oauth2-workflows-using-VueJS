d68aa49582ee72e6cd77cbd6e697f0e02a09320b
Glareone <enlarius@gmail.com>
2019-09-22 19:47:22 +0300

    page structure based on grid

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,70 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="page">
+      <app-header />
+      <app-selection-panel />
+      <app-form/>
+      <app-information-block/>
+      <app-logging/>
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+  import Header from './page-components/Header.vue';
+
+  import Form from './components/Form.vue';
+  import InformationBlock from './components/InformationBlock.vue';
+  import Logging from './components/Logging.vue';
+  import SelectionPanel from './components/SelectionPanel.vue';
+
+  export default {
+    components: {
+      appHeader: Header,
+      appForm: Form,
+      appInformationBlock: InformationBlock,
+      appLogging: Logging,
+      appSelectionPanel: SelectionPanel,
     }
   }
-}
+</script>
+
+<style lang="scss">
+  :root {
+    --grid-gap: 1rem;
+    --padding: 1.5rem;
+    --border-button-radius: 4rem;
+    --block-radius: 1rem;
+    --box-shadow-color: rgba(153,153,153, .8);
+
+    --main-color: #626d71;
+    --main-element-color: #cdcdc0;
+    --secondary-color: #DDBC95;
+    --tertiary-color: #B38867;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    background-color: var(--main-color);
+
+    display: grid;
+    grid-template-columns: minmax(min-content, 90%);
+    justify-content: center;
+    position: relative;
+  }
+
+  .page {
+    display: grid;
+    padding: var(--padding);
+    min-height: 90vh;
+    align-self: center;
+
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: var(--grid-gap);
+    grid-template-rows: [page-start] 10vh [select-panel-start] 10vh [form-start] 20vh [logging-start] minmax(20vh, 1fr) [page-end];
+  }
+
 </style>
---
+++ src/components/Form.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="form-panel">
+    <h3>Form:</h3>
+    <input type="text">
+    <input type="text">
+    <label for="checkbox">Use default values?</label>
+    <input type="checkbox" id="checkbox">
+    <button>Get Code</button>
+    <button>Get Token</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Form'
+  }
+</script>
+
+<style lang="scss" scoped>
+.form-panel {
+  background-color: var(--main-element-color);
+  grid-column: 1 / 3;
+  grid-row: form-start / logging-start;
+  padding: 1rem;
+  border-radius: var(--block-radius);
+  border: 1px solid darkgrey;
+  -webkit-box-shadow: 3px 2px 6px 0 var(--box-shadow-color);
+  -moz-box-shadow: 3px 2px 6px 0 var(--box-shadow-color);
+  box-shadow: 3px 2px 6px 0 var(--box-shadow-color);
+
+}
+</style>
