ef8e5d541632dcfcde3d401e77b36479e75a2715
Glareone <enlarius@gmail.com>
2019-10-07 17:26:17 +0300

    little fix

--- src/components/InformationBlock.vue
+++ src/components/InformationBlock.vue
@@ -1,7 +1,7 @@
 <template>
 
   <div class="info-block">
-    <h3>Information Block</h3>
+    <h3>Information Block:</h3>
     <!--
     use dynamic component.
     https://github.com/Glareone/VueJS-2-Portfolio/blob/master/lectures/lecture-block-9%20Slots%2C%20Dynamic%20Components%2C%20keep-alive%20%2B%20lifecycle%20hooks/src/App.vue
